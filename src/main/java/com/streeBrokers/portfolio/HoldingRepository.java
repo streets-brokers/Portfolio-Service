f8592a075772c7b700dfcc879c6f186833137c31
ellieGem <eaxolu8@gmail.com>
2021-12-15 10:28:03 +0000

    update env

--- src/main/java/com/streeBrokers/portfolio/HoldingRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.streeBrokers.portfolio;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface HoldingRepository extends JpaRepository<Holding, Long> {
-//}
